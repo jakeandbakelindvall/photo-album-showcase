3135a57f765112e143ab2bba7fab77e1975e6a7c
Jake Lindvall <jakeandbakelindvall@gmail.com>
2023-11-13 12:29:06 -0600

    app, photo list, and photo list card styles to look purty

--- src/components/PhotoList/PhotoList.tsx
+++ src/components/PhotoList/PhotoList.tsx
@@ -7,15 +7,20 @@ const PhotoList = ({
   dataTestId,
 }: { photos: Photo[] } & TestTarget) => {
   return (
-    <div data-testid={dataTestId}>
+    <div
+      className="mt-8 flex w-full flex-col justify-start border-t-2 border-orange-300"
+      data-testid={dataTestId}
+    >
       <div className="my-2 font-bold">Results:</div>
-      {photos.map((photo) => (
-        <PhotoListCard
-          key={`photo-list-card-${photo.id}`}
-          photo={photo}
-          dataTestId="photo-list-card"
-        />
-      ))}
+      <div className="grid auto-rows-fr grid-cols-1 gap-1 sm:grid-cols-2 sm:gap-2 lg:grid-cols-3">
+        {photos.map((photo) => (
+          <PhotoListCard
+            key={`photo-list-card-${photo.id}`}
+            photo={photo}
+            dataTestId="photo-list-card"
+          />
+        ))}
+      </div>
     </div>
   );
 };
