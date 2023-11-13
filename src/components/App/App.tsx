46f999ace342a32ffa3c85aaf51361a0ecc2a548
Jake Lindvall <jakeandbakelindvall@gmail.com>
2023-11-13 02:24:56 -0600

    simple input error handling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,7 @@ import PhotoList from "../PhotoList/PhotoList";
 const App = () => {
   const [inputValue, setInputValue] = useState<string>("");
   const [queryValue, setQueryValue] = useState<string>("");
+  const [invalid, setInvalid] = useState<boolean>(false);
 
   const { isLoading, error, data } = useQuery<Photo[]>(
     ["getPhotosByAlbumId", queryValue],
@@ -18,14 +19,19 @@ const App = () => {
   );
 
   const handleSearch = (): void => {
-    setQueryValue(inputValue);
-    setInputValue("");
+    if (/^[1-9][0-9]*$/.test(inputValue)) {
+      setInvalid(false);
+      setQueryValue(inputValue);
+      setInputValue("");
+    } else {
+      setInvalid(true);
+    }
   };
 
   return (
     <div className="flex h-fit min-h-screen items-center justify-center">
       <div className="min-h-[50vh] w-5/6 rounded-2xl border-2 border-slate-800 bg-slate-700 p-8 text-orange-100 shadow-md shadow-blue-950">
-        <h1 className="text-center text-xl">Photo Album Showcase</h1>
+        <h1 className="text-center text-2xl">Photo Album Showcase</h1>
         <Input
           placeholder="Enter Album ID"
           value={inputValue}
@@ -34,6 +40,17 @@ const App = () => {
         <Button disabled={!inputValue.length} onClick={handleSearch}>
           Submit
         </Button>
+        <div>
+          {invalid && (
+            <span
+              className="font-bold text-red-400"
+              data-testid="error-message"
+            >
+              Error:&nbsp;
+            </span>
+          )}
+          Please input a positive integer album ID, without leading zeroes
+        </div>
         <PhotoList photos={data ?? []} />
       </div>
     </div>
